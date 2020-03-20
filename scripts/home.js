2176235a153610a7d306ee5d3f2310401d395fb9
richa <richa1998.patel@gmail.com>
2020-03-20 02:07:14 -0400

    design over

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,5 @@
 
-  var myIndex = 0;
-  carousel();
+
   var nav = document.getElementById("mySidenav");
   var isNavOpen = false;
 
@@ -13,17 +12,6 @@
     }
   }
 
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 5000); // Change image every 2 seconds
-  }
 
 
   function openNav() {
